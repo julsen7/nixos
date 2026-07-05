fff3dea745ad2803fe786ca45477992ff668af71
Julsen <263753131+julsen7@users.noreply.github.com>
2026-07-05 22:21:59 +0200

    Add NixOS configuration for desktop environment

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,1 +1,127 @@
+{ config, lib, pkgs, ... }:
 
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # BOOTLOADER
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # GENERAL
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+
+  zramSwap.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "de_DE.UTF-8";
+  console.keyMap = "de";
+
+  services.xserver.xkb = {
+    layout = "de";
+    options = "eurosign:e,caps:escape";
+  };
+
+  # HARDWARE
+
+  services.xserver.videoDrivers = [
+    "amdgpu"
+    "nvidia"
+  ];
+
+  hardware = {
+    enableAllFirmware = true; # ?
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      open = true;
+      prime = {
+        offload.enable = true;
+        amdgpuBusId = "PCI:5@0:0:0";
+        nvidiaBusId = "PCI:1@0:0:0";
+      };
+    };
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Experimental = true;
+          FastConnectable = true;
+        };
+        Policy = {
+          AutoEnable = true;
+        };
+      };
+    };
+  };
+
+  # SERVICES
+
+  services.openssh.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.ratbagd.enable = true;
+  services.udisks2.enable = true;
+  services.fwupd.enable = true;
+  services.power-profiles-daemon.enable = true;
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # USER
+
+  users.users.julsen = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    hashedPassword = "$y$j9T$n8yEDLyG5/IORRV5SPJ5I.$KEdyBgQbDYMSWWxeZYgW/NpdKltwuBk7RZU7ydNzb5.";
+  };
+
+  security.rtkit.enable = true; # ?
+
+  # PACKAGES
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+  ];
+
+  programs.firefox.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  boot.supportedFilesystems = [ "ntfs" ];
+  security.polkit.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  programs.hyprlock.enable = true;
+
+  system.stateVersion = "26.05";
+}
