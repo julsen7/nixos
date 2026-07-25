acb8bff54b6cc04a8d87fdc2f82285174d06f8ad
julsen7 <263753131+julsen7@users.noreply.github.com>
2026-07-25 15:56:54 +0200

    c

--- home.nix
+++ home.nix
@@ -222,98 +222,6 @@ in {
     };
   };
 
-  programs.vscode = {
-    enable = true;
-    profiles.default = {
-      userSettings = {
-        "explorer.confirmDelete" = false;
-        "workbench.iconTheme" = "material-icon-theme";
-        "workbench.secondarySideBar.defaultVisibility" = "hidden";
-        "explorer.confirmDragAndDrop" = false;
-        "git.confirmSync" = false;
-        "git.autofetch" = true;
-        "editor.fontFamily" = "'JetBrainsMono Nerd Font Propo', 'Droid Sans Mono', monospace";
-        "explorer.confirmPasteNative" = false;
-        "latex-workshop.latex.autoBuild.run" = "onFileChange";
-        "files.autoSave" = "afterDelay";
-        "workbench.startupEditor" = "none";
-        "files.exclude" = {
-          "**/.git" = false;
-        };
-        "latex-workshop.formatting.latex" = "tex-fmt";
-        "github.copilot.enable" = {
-          "*" = false;
-          "plaintext" = false;
-          "markdown" = false;
-          "scminput" = false;
-        };
-        "workbench.colorTheme" = "GitHub Dark Default";
-        "Lua.workspace.library" = [
-          "/usr/share/hypr/stubs"
-        ];
-        "files.simpleDialog.enable" = true;
-      };
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        cweijan.dbclient-jdbc
-        cweijan.vscode-database-client2
-        davidanson.vscode-markdownlint
-        eamodio.gitlens
-        ecmel.vscode-html-css
-        github.github-vscode-theme
-        haskell.haskell
-        james-yu.latex-workshop
-        ms-python.debugpy
-        ms-python.python
-        ms-python.vscode-pylance
-        ms-vscode.cmake-tools
-        ms-vscode.cpptools
-        ms-vscode.cpptools-extension-pack
-        pkief.material-icon-theme
-        redhat.java
-        ritwickdey.liveserver
-        sumneko.lua
-        tamasfe.even-better-toml
-        vscjava.vscode-gradle
-        vscjava.vscode-java-debug
-        vscjava.vscode-java-dependency
-        vscjava.vscode-java-pack
-        vscjava.vscode-java-test
-        vscjava.vscode-maven
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "language-haskell";
-          publisher = "haskell";
-          version = "3.8.0";
-          sha256 = "sha256-wDGvGKI+YDwkbYKV0ijnB3+NwWPZAuwLN4MpFV37KFs=";
-        }
-        {
-          name = "cpp-devtools";
-          publisher = "ms-vscode";
-          version = "0.5.13";
-          sha256 = "sha256-g8ZXdEgKB6okJEVXvFQMGz5oDMsOh5mWzl50B/etVjw=";
-        }
-        {
-          name = "cpptools-themes";
-          publisher = "ms-vscode";
-          version = "2.0.0";
-          sha256 = "sha256-YWA5UsA+cgvI66uB9d9smwghmsqf3vZPFNpSCK+DJxc=";
-        }
-        {
-          name = "prolog";
-          publisher = "rebornix";
-          version = "0.0.4";
-          sha256 = "sha256-SZAaG3dFlDbA46s+i36CMBOU5vJ+1bgTgk+TTyi+yhA=";
-        }
-        {
-          name = "logo-lang";
-          publisher = "zion-school";
-          version = "0.8.1";
-          sha256 = "sha256-LvM51DKHa9G1/EAelCnB0jQL7bRBLvAgYDHQR/MR6Xc=";
-        }
-      ];
-    };
-  };
 
   services.dunst = {
     enable = true;
@@ -818,6 +726,99 @@ in {
         file_manager = "${pkgs.kitty}/bin/kitty -e ${pkgs.yazi}/bin/yazi";
         terminal = "${pkgs.kitty}/bin/kitty";
       };
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    profiles.default = {
+      userSettings = {
+        "explorer.confirmDelete" = false;
+        "workbench.iconTheme" = "material-icon-theme";
+        "workbench.secondarySideBar.defaultVisibility" = "hidden";
+        "explorer.confirmDragAndDrop" = false;
+        "git.confirmSync" = false;
+        "git.autofetch" = true;
+        # "editor.fontFamily" = "'JetBrainsMono Nerd Font Propo', 'Droid Sans Mono', monospace";
+        "explorer.confirmPasteNative" = false;
+        "latex-workshop.latex.autoBuild.run" = "onFileChange";
+        "files.autoSave" = "afterDelay";
+        "workbench.startupEditor" = "none";
+        "files.exclude" = {
+          "**/.git" = false;
+        };
+        "latex-workshop.formatting.latex" = "tex-fmt";
+        "github.copilot.enable" = {
+          "*" = false;
+          "plaintext" = false;
+          "markdown" = false;
+          "scminput" = false;
+        };
+        "workbench.colorTheme" = "GitHub Dark Default";
+        "Lua.workspace.library" = [
+          "/usr/share/hypr/stubs"
+        ];
+        "files.simpleDialog.enable" = true;
+      };
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        cweijan.dbclient-jdbc
+        cweijan.vscode-database-client2
+        davidanson.vscode-markdownlint
+        eamodio.gitlens
+        ecmel.vscode-html-css
+        github.github-vscode-theme
+        haskell.haskell
+        james-yu.latex-workshop
+        ms-python.debugpy
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-vscode.cmake-tools
+        ms-vscode.cpptools
+        ms-vscode.cpptools-extension-pack
+        pkief.material-icon-theme
+        redhat.java
+        ritwickdey.liveserver
+        sumneko.lua
+        tamasfe.even-better-toml
+        vscjava.vscode-gradle
+        vscjava.vscode-java-debug
+        vscjava.vscode-java-dependency
+        vscjava.vscode-java-pack
+        vscjava.vscode-java-test
+        vscjava.vscode-maven
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "language-haskell";
+          publisher = "haskell";
+          version = "3.8.0";
+          sha256 = "sha256-wDGvGKI+YDwkbYKV0ijnB3+NwWPZAuwLN4MpFV37KFs=";
+        }
+        {
+          name = "cpp-devtools";
+          publisher = "ms-vscode";
+          version = "0.5.13";
+          sha256 = "sha256-g8ZXdEgKB6okJEVXvFQMGz5oDMsOh5mWzl50B/etVjw=";
+        }
+        {
+          name = "cpptools-themes";
+          publisher = "ms-vscode";
+          version = "2.0.0";
+          sha256 = "sha256-YWA5UsA+cgvI66uB9d9smwghmsqf3vZPFNpSCK+DJxc=";
+        }
+        {
+          name = "prolog";
+          publisher = "rebornix";
+          version = "0.0.4";
+          sha256 = "sha256-SZAaG3dFlDbA46s+i36CMBOU5vJ+1bgTgk+TTyi+yhA=";
+        }
+        {
+          name = "logo-lang";
+          publisher = "zion-school";
+          version = "0.8.1";
+          sha256 = "sha256-LvM51DKHa9G1/EAelCnB0jQL7bRBLvAgYDHQR/MR6Xc=";
+        }
+      ];
     };
   };
 
