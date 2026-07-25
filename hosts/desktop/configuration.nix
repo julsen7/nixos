c2bd02db51ae000184bc70ed2edaf5bbce0db958
julsen7 <263753131+julsen7@users.noreply.github.com>
2026-07-25 16:09:36 +0200

    ch

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -133,7 +133,7 @@
 
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    image = ../../wallpaper/AssassinsCreed.jpg
   };
 
   # PACKAGES
