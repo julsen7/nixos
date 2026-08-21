8cdb9f6b6bd5b6ffa506ad4f9d07cbcae98debad
julsen7 <263753131+julsen7@users.noreply.github.com>
2026-08-21 21:04:30 +0200

    ch

--- home.nix
+++ home.nix
@@ -166,12 +166,12 @@ in {
     
     # Theme Symlinks
 
-    "dunst/dunstrc".source = ./theme/dunstrc;
-    "hypr/colors.lua".source = ./theme/hypr.lua;
-    "hypr/colors.conf".source = ./theme/hypr.conf;
-    "kitty/current-theme.conf".source = ./theme/kitty.conf;
-    "rofi/colors.rasi".source = ./theme/rofi.rasi;
-    "waybar/colors.css".source = ./theme/waybar.css;
+    "dunst/dunstrc".source = ~/theme/dunstrc;
+    "hypr/colors.lua".source = ~/theme/hypr.lua;
+    "hypr/colors.conf".source = ~/theme/hypr.conf;
+    "kitty/current-theme.conf".source = ~/theme/kitty.conf;
+    "rofi/colors.rasi".source = ~/theme/rofi.rasi;
+    "waybar/colors.css".source = ~/theme/waybar.css;
   };
 
   home.activation.initTheme = lib.hm.dag.entryAfter ["writeBoundary"] ''
@@ -182,12 +182,12 @@ in {
 
       mkdir -p "$HOME/.config/hypr"
 
-      DEFAULT_WALLPAPER="${./wallpaper/AssassinsCreed.jpg}"
+      DEFAULT_WALLPAPER="$HOME/wallpaper/AssassinsCreed.jpg"
 
       if [ -f "$DEFAULT_WALLPAPER" ]; then
         cp "$DEFAULT_WALLPAPER" "$HOME/.config/hypr/current_wallpaper"
 
-        ${pkgs.matugen}/bin/matugen image "$DEFAULT_WALLPAPER" --source-color-index 0
+        matugen image "$DEFAULT_WALLPAPER" --source-color-index 0
 
         touch "$FLAG_FILE"
         echo "Default wallpapaer and theme initialized!"
